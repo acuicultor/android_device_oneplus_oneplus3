bf40eca9b4687041c8dadb6486c8feed9208a444
acuicultor <ramoncuenca4@gmail.com>
2018-01-05 09:13:17 +0100

    Add fix for common jack issues

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+# Add fix for common JACK issues
+ANDROID_JACK_VM_ARGS := -Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
